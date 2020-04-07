c7311fb20efb37750e5870cd0fb6b7af6e191149
Shankara <=>
2020-04-07 17:34:08 +0530

    Moved count variable of display_notes function outside the for loop

--- atm.c
+++ atm.c
@@ -4,8 +4,9 @@
 void display_notes(unsigned int notes) { 
   unsigned short denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned int denomination_pos = 0xf0000000;
+  unsigned int count = 0;
   for (int i=0; i<8; i++) {
-    unsigned count = notes & denomination_pos;
+    count = notes & denomination_pos;
     count && printf("%d notes of Rs. %d\n", (count >> ((7-i)*4)), denominations[i]);
     denomination_pos = denomination_pos >> 4;
   }
