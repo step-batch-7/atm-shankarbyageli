cd00f4071af18610622385548089eda34effa728
Shankara <=>
2020-04-08 07:30:39 +0530

    Modified display_notes function of atm.c
    Now it prints "note" for 1 note and
    "notes" for more than 1 note.

--- atm.c
+++ atm.c
@@ -6,13 +6,15 @@ void display_notes(unsigned int notes) {
   unsigned int count = 0;
   for (int i=0; i<8; i++) {
     count = (notes << (i*4)) >> 28;
-    count && printf("%d notes of Rs. %d\n", count, denominations[i]);
+    count && printf("%d %s of Rs. %d\n", count, count > 1 ? "notes" : "note", denominations[i]);
   }
 }
 
 unsigned int get_money(unsigned short amount) {
-  unsigned int notes_count = 0x00000000;
-  if(amount > 31999) return notes_count;
+  unsigned int notes_count = 0x0;
+  if(amount > 31999) {
+    return notes_count;
+  }
   unsigned short denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
   for (int i=0; i<8; i++) {
     notes_count <<= 4;
