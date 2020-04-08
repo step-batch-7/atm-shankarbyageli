04d5d78fe1621e04f02cd604ba186c97edd92c7d
Shankara <=>
2020-04-08 07:16:04 +0530

    Modified display_notes function of atm.c

--- atm.c
+++ atm.c
@@ -3,12 +3,10 @@
 
 void display_notes(unsigned int notes) { 
   unsigned short denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
-  unsigned int denomination_pos = 0xf0000000;
   unsigned int count = 0;
   for (int i=0; i<8; i++) {
-    count = notes & denomination_pos;
-    count && printf("%d notes of Rs. %d\n", (count >> ((7-i)*4)), denominations[i]);
-    denomination_pos = denomination_pos >> 4;
+    count = (notes << (i*4)) >> 28;
+    count && printf("%d notes of Rs. %d\n", count, denominations[i]);
   }
 }
 
@@ -17,8 +15,8 @@ unsigned int get_money(unsigned short amount) {
   if(amount > 31999) return notes_count;
   unsigned short denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
   for (int i=0; i<8; i++) {
-    notes_count = notes_count << 4;
-    notes_count += amount / denominations[i];
+    notes_count <<= 4;
+    notes_count |= amount / denominations[i];
     amount %= denominations[i];
   }
   return notes_count;
